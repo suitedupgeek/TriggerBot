952ec7bd07a344c0a7c6670b25b4dc68bfdc0f93
Johnathan Williamson <johnathan_williamson@technologyonecorp.com>
2021-07-05 17:48:50 +0100

    Typo fix

--- TriggerBot.py
+++ TriggerBot.py
@@ -166,7 +166,7 @@ def listener2(messages):
         if(get_activity(m.chat.id)):
             get_activity(m.chat.id)[name] = time.strftime("%Y-%m-%d %H:%M:%S")
         else:
-            activity[str(m.chat.id)] = {name: time.strftime("%Y-%m-%d %H:%M:%S"}
+            activity[str(m.chat.id)] = {name: time.strftime("%Y-%m-%d %H:%M:%S")}
         
         save_activity()
 
