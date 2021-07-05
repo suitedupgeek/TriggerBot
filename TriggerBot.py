17c09a86f614511b6e2e8697be5cfeee8744d62e
Johnathan Williamson <johnathan_williamson@technologyonecorp.com>
2021-07-05 20:15:25 +0100

    tabs

--- TriggerBot.py
+++ TriggerBot.py
@@ -504,7 +504,7 @@ def activityLog(m):
             for member in act:
                 global FullList
                 memberTime = act.values()
-                lineItem = '{} : {} \n'.format(member, memberTime[listIndex])
+                lineItem = '{} \t\t: {} \n'.format(member, memberTime[listIndex])
                 bot.send_message(m.chat.id, lineItem)
                 FullList += lineItem
                 listIndex += 1
