27a375af30290dda99011557c4f13c3df9d276be
Johnathan Williamson <johnathan_williamson@technologyonecorp.com>
2021-07-05 18:45:01 +0100

    activity command added

--- TriggerBot.py
+++ TriggerBot.py
@@ -486,7 +486,10 @@ def roll(m):
 @bot.message_handler(commands=['ping'])
 def ping(m):
     bot.send_message(m.chat.id,"Yes yes yes. Here I am, brain the size of a planet, and they tell me to repeat things for you and roll dice.")
-        
+
+@bot.message_handler(commands=['activity','Activity'])
+def activity(m):
+    bot.send_message(m.chat.id,"Yep ok first stage done.")      
 
 # END OF COMMAND IMPLEMENTATION SECTION.
 
