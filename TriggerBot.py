4762e8ec0d8b0b2be0d63d5ccf19726a9d827610
Johnathan Williamson <johnathan_williamson@technologyonecorp.com>
2021-07-06 17:43:24 +0100

    prettyDate

--- TriggerBot.py
+++ TriggerBot.py
@@ -33,6 +33,49 @@ triggers = {}
 # Separator character.
 separator = '->'
 
+# Pretty Date Generator
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + " months ago"
+    return str(day_diff / 365) + " years ago"
 
 # Check if a message is too old.
 def is_recent(m):
@@ -528,19 +571,7 @@ def activityLog(m):
             for member in act:
                 memberTime = act.values()
                 memberTimeObject = datetime.strptime(memberTime[listIndex], '%Y-%m-%d %H:%M')
-                
-                if memberTimeObject > recently:
-                    friendlyTime = "Seen Recently"
-                elif memberTimeObject < twodays:
-                    friendlyTime = "At least 2 days"
-                elif memberTimeObject < threedays:
-                    friendlyTime = "At least 3 days"
-                elif memberTimeObject < fourdays:
-                    friendlyTime = "At least 4 days"
-                elif memberTimeObject < morethan:
-                    friendlyTime = "At least 5 days"
-                elif memberTimeObject < ages:
-                    friendlyTime = "More than 5 days"
+                prettyDate = pretty_date(memberTimeObject)
 
                 try:
                     # Get user detail - if it still exists in the room move on.
@@ -557,14 +588,14 @@ def activityLog(m):
                     #if detailMember.status in ("member","creator","administrator"):
                             if (detailMember.user.last_name == None):
                                 detailMember.user.last_name = " "
-                            lineItem = '{0:10} \t: {1} {2} {3} \n'.format(friendlyTime, detailMember.user.first_name, detailMember.user.last_name, detailMember.user.id)
+                            lineItem = '{0:10} \t: {1} {2} {3} \n'.format(prettyDate, detailMember.user.first_name, detailMember.user.last_name, detailMember.user.id)
                             FullList += lineItem
                 elif (behaviour > 0):
                     #if detailMember.status in ("member","creator","administrator"):
                         if (memberTimeObject < targetTime):
                                 if (detailMember.user.last_name == None):
                                     detailMember.user.last_name = " "
-                                lineItem = '{0:10} \t: {1} {2} {3} \n'.format(friendlyTime, detailMember.user.first_name, detailMember.user.last_name, detailMember.user.id)
+                                lineItem = '{0:10} \t: {1} {2} {3} \n'.format(prettyDate, detailMember.user.first_name, detailMember.user.last_name, detailMember.user.id)
                                 FullList += lineItem
                 listIndex += 1
 
