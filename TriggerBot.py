8b90b69869ee0adf6ec4ff289804b571334e2675
Johnathan Williamson <johnathan_williamson@technologyonecorp.com>
2021-07-06 15:02:12 +0100

    reset detailMember

--- TriggerBot.py
+++ TriggerBot.py
@@ -531,6 +531,7 @@ def activityLog(m):
                 lineItem = '{0:10} \t: {1} {1} \n'.format(memberTime[listIndex], detailMember.user.first_name, detailMember.user.last_name)
                 FullList += lineItem
                 listIndex += 1
+                detailMember = None
             
             bot.send_message(m.chat.id, FullList)
 
