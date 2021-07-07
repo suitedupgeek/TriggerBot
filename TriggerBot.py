fd1e8b4f36b01bdc4205d7a3e6db8e4b7c29fadb
Johnathan Williamson <johnathan_williamson@technologyonecorp.com>
2021-07-07 13:33:19 +0100

    debug

--- TriggerBot.py
+++ TriggerBot.py
@@ -573,41 +573,40 @@ def activityLog(m):
                 print('Member \n')
                 print(members)
                 print('\n')
-                for member in members:
-                    for value in member:
-                        print('Member Time \n')
-                        print(value)
-                        print('\n')
+                for value in members:
+                    print('Member Time \n')
+                    print(value[1])
+                    print('\n')
 
-                        memberTime = value
-                        memberTimeObject = datetime.strptime(memberTime, '%Y-%m-%d %H:%M:%S')
-                        prettyDate = pretty_date(memberTimeObject)
-                        try:
-                            # Get user detail - if it still exists in the room move on.
-                            detailMember = bot.get_chat_member(m.chat.id,member)
-                            #print(detailMember)
-                        except:
-                            # User not found in the room, set status to left
-                            detailMember.status = 'left'
-                            print('User not found: ' + member + 'setting status to left')
-                        
-                        # Remove users from activity.json if status is 'left' or 'kicked' 
-                        # TODO need to actually make this happen rather than just ignoring them.
+                    memberTime = value
+                    memberTimeObject = datetime.strptime(memberTime, '%Y-%m-%d %H:%M:%S')
+                    prettyDate = pretty_date(memberTimeObject)
+                    try:
+                        # Get user detail - if it still exists in the room move on.
+                        detailMember = bot.get_chat_member(m.chat.id,member)
+                        #print(detailMember)
+                    except:
+                        # User not found in the room, set status to left
+                        detailMember.status = 'left'
+                        print('User not found: ' + member + 'setting status to left')
+                    
+                    # Remove users from activity.json if status is 'left' or 'kicked' 
+                    # TODO need to actually make this happen rather than just ignoring them.
 
 
-                        if (behaviour == 0):
-                            #if detailMember.status in ("member","creator","administrator"):
+                    if (behaviour == 0):
+                        #if detailMember.status in ("member","creator","administrator"):
+                                if (detailMember.user.last_name == None):
+                                    detailMember.user.last_name = " "
+                                lineItem = '{0:10} \t: {1} {2}\n'.format(prettyDate, detailMember.user.first_name, detailMember.user.last_name)
+                                FullList += lineItem
+                    elif (behaviour > 0):
+                        #if detailMember.status in ("member","creator","administrator"):
+                            if (memberTimeObject < targetTime):
                                     if (detailMember.user.last_name == None):
                                         detailMember.user.last_name = " "
                                     lineItem = '{0:10} \t: {1} {2}\n'.format(prettyDate, detailMember.user.first_name, detailMember.user.last_name)
                                     FullList += lineItem
-                        elif (behaviour > 0):
-                            #if detailMember.status in ("member","creator","administrator"):
-                                if (memberTimeObject < targetTime):
-                                        if (detailMember.user.last_name == None):
-                                            detailMember.user.last_name = " "
-                                        lineItem = '{0:10} \t: {1} {2}\n'.format(prettyDate, detailMember.user.first_name, detailMember.user.last_name)
-                                        FullList += lineItem
                 listIndex += 1
 
             bot.send_message(m.chat.id, FullList)
